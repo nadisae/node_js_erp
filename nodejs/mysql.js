6c84dde5f539d6bc15e826f2bf291a22cab0d9e8
강정구 <gangjeong-gu@gangjeong-gus-iMac.local>
2020-06-17 08:42:07 +0900

    ver -0.02

--- nodejs/mysql.js
+++ nodejs/mysql.js
@@ -1,21 +1,9 @@
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'my_db'
-});
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'erp_admin',
+  password: 'sork1emd',
+  database: 'erp'
+   });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
-});
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
-
-connection.end();
+  connection.connect();
